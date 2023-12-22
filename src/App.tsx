5bc9aefa5f1d6ae1543ef4f8daec8f5dd28601b0
Sumee Pokharel <sumeepokharel@Sumees-MacBook-Pro.local>
2023-12-22 17:36:06 -0600

    final css added

--- src/App.tsx
+++ src/App.tsx
@@ -1,71 +1,11 @@
-import React, { useState } from "react";
 import TaskList from "./component/TaskList";
-import TaskForm from "./component/TaskForm";
-import CRUDOperations from "./component/CRUDOperations";
-import styles from "./App.module.css";
 
-interface Task {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-const App: React.FC = () => {
-  const [tasks, setTasks] = useState<Task[]>([]);
-  const [newTaskTitle, setNewTaskTitle] = useState("");
-
-  const [editingTaskId, setEditingTaskId] = useState<number | null>(null);
-
-  const addTask = (title: string) => {
-    const newTask = {
-      userId: 1,
-      id: tasks.length + 1,
-      title: title,
-      completed: false,
-    };
-
-    setTasks((prevTasks) => [...prevTasks, newTask]);
-    setNewTaskTitle("");
-  };
-
-  const editTask = (taskId: number) => {
-    setEditingTaskId(taskId);
-    const taskToEdit = tasks.find((task) => task.id === taskId);
-    if (taskToEdit) {
-      setNewTaskTitle(taskToEdit.title);
-    }
-  };
-
-  const updateTask = () => {
-    if (editingTaskId !== null) {
-      setTasks((prevTasks) =>
-        prevTasks.map((task) =>
-          task.id === editingTaskId ? { ...task, title: newTaskTitle } : task
-        )
-      );
-      setNewTaskTitle("");
-      setEditingTaskId(null);
-    }
-  };
-
-  const deleteTask = (taskId: number) => {
-    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
-  };
-
+function App() {
   return (
-    <div>
-      <div className={styles.appContainer}>
-        <h1>Task Manager</h1>
-        <TaskList task={tasks} />
-        <TaskForm addTask={addTask} />
-        <CRUDOperations
-          editTask={editTask}
-          deleteTask={deleteTask}
-          updateTask={updateTask}
-        />
-      </div>
-    </div>
+    <>
+      <TaskList />
+    </>
   );
-};
+}
 
 export default App;
--- src/component/TaskList.tsx
+++ src/component/TaskList.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "../App.module.css";
 
@@ -9,37 +9,142 @@ interface Task {
   completed: boolean;
 }
 
-interface TaskListProps {
-  task: Task[];
-}
-
-const TaskList: React.FC<TaskListProps> = ({ task }) => {
+const TaskList: React.FC = () => {
   const [tasks, setTasks] = useState<Task[]>([]);
+  const [newTask, setNewTask] = useState<string>("");
+  const [editingTask, setEditingTask] = useState<number | null>(null);
+  const [editedTask, setEditedTask] = useState<string>("");
 
   useEffect(() => {
-    // Fetch tasks
     async function getTasks() {
-      const response = await axios(
-        "https://jsonplaceholder.typicode.com/todos/"
-      );
-      setTasks(response.data);
+      try {
+        const response = await axios(
+          "https://jsonplaceholder.typicode.com/todos/"
+        );
+        setTasks(response.data);
+      } catch (error) {
+        console.error("Error fetching tasks:", error);
+      }
     }
 
     getTasks();
   }, []);
 
+  const handleAddTask = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (newTask.trim() !== "") {
+      const newTaskObject: Task = {
+        userId: 1,
+        id: tasks.length + 1,
+        title: newTask,
+        completed: true,
+      };
+      setTasks([...tasks, newTaskObject]);
+      setNewTask("");
+    }
+  };
+
+  const handleEditTask = (taskId: number) => {
+    setEditingTask(taskId);
+    const taskToEdit = tasks.find((task) => task.id === taskId);
+    setEditedTask(taskToEdit ? taskToEdit.title : "");
+  };
+
+  const handleSaveEdit = (taskId: number) => {
+    const updatedTasks = tasks.map((task) =>
+      task.id === taskId ? { ...task, title: editedTask } : task
+    );
+    setTasks(updatedTasks);
+    setEditingTask(null);
+    setEditedTask("");
+  };
+
+  const handleDeleteTask = (taskId: number) => {
+    setTasks((prevTasks) => {
+      const updatedTasks = prevTasks.filter((task) => task.id !== taskId);
+      setEditingTask(null);
+      return updatedTasks;
+    });
+  };
+
   return (
-    <div>
-      <h2>Task List</h2>
-      <ul>
-        {tasks.map((task) => (
-          <li key={task.id} className={styles.taskItem}>
-            {task.title} {task.id} {task.completed ? "Completed" : "Incomplete"}
-            <div className={styles.taskButtons}></div>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <fieldset className={styles.container}>
+        <form onSubmit={handleAddTask} className={styles.addTask}>
+          <input
+            className={styles.inputBox}
+            type="text"
+            placeholder="Enter task here"
+            value={newTask}
+            onChange={(e) => setNewTask(e.target.value)}
+          />
+          <button type="submit" className={styles.addButton}>
+            Add New Task
+          </button>
+        </form>
+
+        <div>
+          <h2>Todo List</h2>
+          <ul className={styles.tasklist}>
+            {tasks
+              .slice()
+              .reverse()
+              .map((task) => (
+                <li key={task.id} className={styles.taskItem}>
+                  {editingTask === task.id ? (
+                    <div className={styles.editForm}>
+                      <input
+                        type="text"
+                        value={editedTask}
+                        onChange={(e) => setEditedTask(e.target.value)}
+                      />
+                      <button
+                        onClick={() => handleSaveEdit(task.id)}
+                        className={styles.saveButton}
+                      >
+                        Save
+                      </button>
+                    </div>
+                  ) : (
+                    <>
+                      <div className={styles.taskInfo}>
+                        <div>ID: {task.id}</div>
+                        <div>Title: {task.title}</div>
+                        <div>
+                          Status:{" "}
+                          <span
+                            className={
+                              task.completed
+                                ? styles.completedStatus
+                                : styles.incompleteStatus
+                            }
+                          >
+                            {task.completed ? "Completed" : "Incomplete"}
+                          </span>
+                        </div>
+                      </div>
+                      <div className={styles.taskActions}>
+                        <button
+                          className={styles.editButton}
+                          onClick={() => handleEditTask(task.id)}
+                        >
+                          Edit
+                        </button>
+                        <button
+                          className={styles.deleteButton}
+                          onClick={() => handleDeleteTask(task.id)}
+                        >
+                          Delete
+                        </button>
+                      </div>
+                    </>
+                  )}
+                </li>
+              ))}
+          </ul>
+        </div>
+      </fieldset>
+    </>
   );
 };
 
